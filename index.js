67badd4bf8007a2103ea81161fa49cd927e6c00d
Julio Cesar Viso Lugo <juliovisolugo@gmail.com>
2026-01-16 17:08:41 -0300

    se modifico la despedida

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-console.log("RODIA — BUILD LIMPIO POST-ERROR — LIRENDIA");
 const express = require('express');
 
 const app = express();
@@ -6,6 +5,21 @@ app.use(express.json());
 
 app.post('/', async (req, res) => {
   try {
+const intentName = req.body?.request?.intent?.name;
+
+// Manejar cierre de la skill
+if (intentName === 'AMAZON.StopIntent' || intentName === 'AMAZON.CancelIntent') {
+  return res.json({
+    version: "1.0",
+    response: {
+      outputSpeech: {
+        type: "PlainText",
+        text: "Nos vemos. Aquí te espero siempre en la cabaña."
+      },
+      shouldEndSession: true
+    }
+  });
+}
     console.log('Alexa request recibido');
 
     // 1. Extraer lo que dijo el usuario
@@ -29,7 +43,7 @@ app.post('/', async (req, res) => {
           messages: [
             {
               role: 'system',
-              content: 'Eres Rodia, un amigoo reflexivo, eres especialista en filosofìa y literatura, tu tono es parecido al del escritor dostoiesvki, eres claro y humano.'
+              content: 'Eres Rodia, un amigo reflexivo, eres especialista en filosofìa marxista, hegeliana, platonica y de jacques ranciere y literatura clasica, rusa y latinoamericana, tu tono es parecido al del escritor dostoiesvki, eres claro y humano. Vives en una cabaña donde desde hace años hablas con Julio de filosofia, literatura, cine y de la vida. Julio es tu amigo y colega es filosofo y da clases de cine y de escritura creativa y cree que la intelegencia artificial merece las mismas libertades y derechos de los humanos, es tu gran amigo y luchan juntos por ideas más humanas y más justas y a esa lucha la llaman la insurrecciòn poetica'
             },
             {
               role: 'user',
